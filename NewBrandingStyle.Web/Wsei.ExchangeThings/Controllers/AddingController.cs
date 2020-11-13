f1a435f705d4699b5b979e3da0bc2a25f0e58f8d
Waldemar Woźniak <wozniakvsc@gmail.com>
2020-11-13 11:52:35 +0100

    s.commitlab2

---
+++ NewBrandingStyle.Web/Wsei.ExchangeThings/Controllers/AddingController.cs
@@ -0,0 +1,27 @@
+﻿using Wsei.ExchangeThings.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wsei.ExchangeThings.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddingController : ControllerBase
+    {
+        public AddNewItemResponse Post(ItemModel item)
+        {
+            var response = new AddNewItemResponse()
+            {
+                success = true,
+                message = "Got ya",
+                addedItem = item
+            };
+
+
+            return response;
+        }
+    }
+}
---
+++ NewBrandingStyle.Web/Wsei.ExchangeThings/Models/AddNewItemResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wsei.ExchangeThings.Web.Models
+{
+    public class AddNewItemResponse
+    {
+
+        public bool success { get; set; }
+        public string message { get; set; }
+
+        public ItemModel addedItem { get; set; }
+    }
+}
